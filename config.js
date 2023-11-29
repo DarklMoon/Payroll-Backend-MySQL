1f8340014fb6064d9db27b7fa2783ef5e4acb17b
DarklMoom <92285604+DarklMoon@users.noreply.github.com>
2023-11-29 09:43:38 +0700

    Delete config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: "project-service.ctyetg9uawza.ap-southeast-2.rds.amazonaws.com",
-  user: "root",
-  password: "ikfIjN-23412ddasd+3d",
-  database: "payroll_service",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
-module.exports = pool;
